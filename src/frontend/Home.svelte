0b2f0d50879856a5d4f8b2956a5e6af553bfe6da
dieruiper <47790082+dieruiper@users.noreply.github.com>
2020-05-06 21:55:09 +0200

    final version Diego

--- src/frontend/Home.svelte
+++ src/frontend/Home.svelte
@@ -1,6 +1,33 @@
 <main>
-	<h3>APIs:</h3>
-	<p><a href="#/books_exportsAPI"> books_exportsAPI </a></p>
-	<p><a href="#/health_publicAPI"> health_publicAPI </a></p>
-	<p><a href="#/life_expectanciesAPI"> life_expectanciesAPI </a></p>
+	
+	<body>
+		<h3>Frontends:</h3>
+	<p><a href="#/books_exportsAPI"> Exportacion libros </a></p>
+	<p><a href="#/health_publicAPI"> Salud pública </a></p>
+	<p><a href="#/life_expectanciesAPI"> Esperanza vida </a></p>
+		<h3>Team</h3>
+		<ul>
+			<li><h6> <a href="https://github.com/dieruiper">Diego Ruiz Perez</a></h6></li>
+			<li><h6> <a href="https://github.com/J5Mario"> Mario Jimenez Santos</a></h6></li>
+			<li><h6> <a href="https://github.com/Kingmathy"> Mathias Abban Ehentam</a></h6></li>
+		</ul>
+	
+		<h3>Project description:</h3>
+		<p>Nuestras fuentes de información están orientadas a analizar las estadisticas sobre la exportación de libros, gasto publico en salud, y esperanza de vida en el ámbito europeo.</p>
+	
+		<h3>Repository:</h3>
+		<a href="https://github.com/gti-sos/SOS1920-05">gti-sos/SOS1920-05</a>
+	
+		<h3>URL:</h3>
+		<a href="http://sos1920-05.herokuapp.com/">http://sos1920-05.herokuapp.com/</a>
+	
+		<h3>APIs:</h3>
+		<ul>
+			<li><a href="https://sos1920-05.herokuapp.com/api/v1/books-exports">[https://sos1920-05.herokuapp.com/api/v1/books-exports]</a><a href=""> Fuente </a> (developed by <a href="https://github.com/dieruiper">Diego Ruiz Pérez)</a></li>
+			
+			<li><a href="https://sos1920-05.herokuapp.com/api/v1/health_public">[https://sos1920-05.herokuapp.com/api/v1/health_public]</a><a href=""> Fuente </a> (developed by <a href="https://github.com/J5Mario">Mario Jimenez Santos)</a></li>
+			
+		   <li><a href="https://sos1920-05.herokuapp.com/api/v1/life_expectancies">[https://sos1920-05.herokuapp.com/api/v1/life_expectancies]</a><a href=""> Fuente </a> (developed by <a href="https://github.com/Kingmathy">Mathias Abban Ehentam)</a></li>
+		</ul>	
+	</body>
 </main>
