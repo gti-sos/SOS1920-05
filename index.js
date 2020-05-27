71060774e63a9f1b37ba5b1195caed2211fb7f2d
dieruiper <47790082+dieruiper@users.noreply.github.com>
2020-05-27 19:42:33 +0200

    cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const cors = require("cors");
 const books_exportsAPI = require(path.join(__dirname,"./src/backend/books_exportsAPI"));
 const health_publicAPI = require(path.join(__dirname,"./src/backend/health_publicAPI"));
 const life_expectanciesAPI = require(path.join(__dirname,"./src/backend/life_expectanciesAPI"));
+
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -11,7 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use("/",express.static("./public"));
 
-
+app.use(cors());
 books_exportsAPI(app);
 health_publicAPI(app);
 life_expectanciesAPI(app);
