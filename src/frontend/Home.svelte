bad6370109bbe97209da27e6b5098b8ecec04e91
J5Mario <56630449+J5Mario@users.noreply.github.com>
2020-05-28 18:21:36 +0200

    update home

--- src/frontend/Home.svelte
+++ src/frontend/Home.svelte
@@ -36,7 +36,7 @@
 		<h3>Awesome Charts:</h3>
 		<ul>
 			<p><a href="https://sos1920-05.herokuapp.com/#/books-exports/graphC3">Apexchart Exportacion Libros</a></p>
-			<p><a href="https://sos1920-05.herokuapp.com/#/health_public/health_publicBillBoard">BillBoard Gasto en Salud</a></p>				
+			<p><a href="https://sos1920-05.herokuapp.com/#/health_public/health_publicChartJS">ChartJS Gasto en Salud</a></p>				
 			
 		</ul>
 		
