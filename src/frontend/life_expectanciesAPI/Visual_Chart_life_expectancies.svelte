0e28f4fa1c59dcba750f133e829e452ab6a82d8a
Kingmathy <mathsbotchey@hotmail.com>
2020-05-31 14:35:04 +0200

    using anycharts

--- src/frontend/life_expectanciesAPI/Visual_Chart_life_expectancies.svelte
+++ src/frontend/life_expectanciesAPI/Visual_Chart_life_expectancies.svelte
@@ -2,7 +2,7 @@
 <script>
     import {pop} from "svelte-spa-router";
     import Button from "sveltestrap/src/Button.svelte";
-    import ApexCharts from 'apexcharts'
+    import Anychart from "anychart";
 
     async function loadGraph() {
   
@@ -10,13 +10,13 @@
     const resData = await fetch("/api/v1/life_expectancies");
     MyData = await resData.json();
     let countries = Array.from(new Set(MyData.filter(d => d.year == 2016).map((d) => {return d.country;})));
-    let women_life_expectancy = Array.from(new Set(MyData.filter(d => d.year == 2016).map((d) => {return d["Women"];})));
-    
-        var options = {
-          series: Women,
+    let women_life_expectancy = Array.from(new Set(MyData.filter(d => d.year == 2016).map((d) => {return d["women_life_expectancy"];})));
+
+    var options = {
+          series: women_life_expectancy,
           chart: {
           width: 600,
-          type: 'cylinder',
+          type: 'funnel',
         },
         labels: countries,
         responsive: [{
@@ -31,32 +31,39 @@
           }
         }]
         };
-
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
+var chart = new Anychart(document.querySelector("#chart"), options);
         chart.render();
-      
+        chart = anychart.funnel(data);
 
-  }
-  
+// set the container id
+chart.container("container");
+
+// initiate drawing the chart
+chart.draw();
+    }
 </script>
 
 
 
 
 <svelte:head>
-    <script src="https://cdn.jsdelivr.net/npm/apexcharts" on:load={loadGraph}></script>
+<script src="https://cdn.anychart.com/releases/8.7.1/js/anychart-core.min.js" on:load={loadGraph}></script>
+<script src="https://cdn.anychart.com/releases/8.7.1/js/anychart-pyramid-funnel.min.js" on:load={loadGraph}></script>
 </svelte:head> 
 
 <main >
     <h3>Esperanza de vida de mujeres en 2016</h3>
     <h6>Gráfica en la que se muestran la esperanza de vida de mujeres en 2016</h6>
-    <div  id="chart" style="min-width: 310px; max-width: 800px; height: 400px; margin:auto"></div>
+    <div id="container"></div>
     <p></p>
     <Button outline color="secondary" on:click="{pop}"> Volver</Button>
 
 </main>
 <style>
-	main {
-		text-align: center;
-	}
+ html, body, #container {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+}
 </style>
