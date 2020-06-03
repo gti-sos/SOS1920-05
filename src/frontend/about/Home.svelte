485b34b65b13be0249a8140e6cd60b8d63628583
J5Mario <56630449+J5Mario@users.noreply.github.com>
2020-06-03 20:25:28 +0200

    .

--- src/frontend/about/Home.svelte
+++ src/frontend/about/Home.svelte
@@ -7,51 +7,24 @@
       </h1>
   </center>
   
-  <head>
-  <style>
-  #customers {
-  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-  }
-  
-  #customers td, #customers th {
-  border: 1px solid #ddd;
-  padding: 8px;
-  }
-  
-  #customers tr:nth-child(even){background-color: #f2f2f2;}
-  
-  #customers tr:hover {background-color: #ddd;}
-  
-  #customers th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #4CAF50;
-  color: white;
-  }
-  </style>
-  </head>
-  
   <br>
   <br>
   
-<main><center>
+<main>
+  <center>
+    <h4>API de Libros</h4>
+    <p>
+    <iframe width="480" height="270" src="https://www.youtube.com/embed/GdlT1KT-oPo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </p>
+    <h4>API de Gastos</h4>
+    <p>
+    <iframe width="480" height="270" src="https://www.youtube.com/embed/GdlT1KT-oPo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </p>
+    <h4>API de Esperanza de vida</h4>
+    <p>
+    <iframe width="480" height="270" src="https://www.youtube.com/embed/GdlT1KT-oPo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </p>
 
-  <h4>API de Libros</h4>
-  <p>
-  <iframe width="480" height="270" src="https://www.youtube.com/embed/GdlT1KT-oPo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  </p>
-  <h4>API de Gastos</h4>
-  <p>
-  <iframe width="480" height="270" src="https://www.youtube.com/embed/GdlT1KT-oPo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  </p>
-  <h4>API de Esperanza de vida</h4>
-  <p>
-  <iframe width="480" height="270" src="https://www.youtube.com/embed/GdlT1KT-oPo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  </p>
-
-  <p><Button outline color="secondary" on:click="{pop}"> Volver</Button></p>
-  
-</center></main>
+    <p><Button outline color="secondary" on:click="{pop}"> Volver</Button></p>
+  </center>
+</main>
